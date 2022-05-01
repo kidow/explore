0b8dcfd105c6c1d6336527ce1cc3ba11d55eedeb
kidow <wcgo2ling@gmail.com>
2022-05-01 20:49:33 +0900

    tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 interface State {}
 
 const HomePage = () => {
-  return <>HomePage</>
+  return (
+    <div className="container mx-auto max-w-4xl px-6 pt-10 pb-20">
+      <div>
+        <img src="/kidow-explore.svg" alt="" className="h-7" />
+      </div>
+    </div>
+  )
 }
 
 export default HomePage
