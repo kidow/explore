d7fda7e9d165e2f939b413270bf51d1f746e8140
kidow <wcgo2ling@gmail.com>
2022-05-09 01:55:47 +0900

    urlMetadata 테스트

--- pages/api/scrap.ts
+++ pages/api/scrap.ts
@@ -1,3 +1,18 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import urlMetadata from 'url-metadata'
 
-export default async (req: NextApiRequest, res: NextApiResponse) => { }
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const LINKS = [
+    'https://dev.to',
+    'https://www.ttimes.co.kr',
+    'https://now.rememberapp.co.kr',
+    'https://medium.com',
+    'https://www.surfit.io'
+  ]
+  try {
+    const metadatas = await Promise.all(LINKS.map(link => urlMetadata(link)))
+    return res.status(200).json(metadatas)
+  } catch (err) {
+    res.status(400).json(err)
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,30 @@
 import Head from 'next/head'
 import { useEffect } from 'react'
+import Link from 'next/link'
+import type { Result } from 'url-metadata'
+import { useObjectState } from 'services'
 
-interface State {}
+interface State {
+  list: Result[]
+  isLoading: boolean
+}
 
 const HomePage = () => {
-  const get = async () => {}
+  const [{ list, isLoading }, setState] = useObjectState<State>({
+    list: [],
+    isLoading: true
+  })
+
+  const get = async () => {
+    try {
+      const res = await fetch('/api/scrap')
+      const data = await res.json()
+      setState({ list: data, isLoading: false })
+    } catch (err) {
+      console.log(err)
+      setState({ isLoading: false })
+    }
+  }
 
   useEffect(() => {
     get()
@@ -14,13 +34,35 @@ const HomePage = () => {
       <Head>
         <title>Explore - Kidow</title>
       </Head>
-      <div className="container mx-auto max-w-4xl px-6 pt-10">
+      <div className="container mx-auto px-4 pt-10">
         <img src="/kidow-explore.svg" alt="" className="h-7" />
       </div>
 
-      <div className="container mx-auto mt-5 max-w-4xl px-6 pb-20">
-        <div className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          <div>asd</div>
+      <div className="container mx-auto mt-5 px-4 pb-20">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+          {isLoading
+            ? '불러오는 중...'
+            : list.map((item, key) => (
+                <Link href={item.url} key={key}>
+                  <a target="_blank" rel="noreferrer">
+                    <div className="h-80 rounded-lg border border-neutral-700">
+                      <img
+                        src={item.image}
+                        alt=""
+                        className="h-40 w-full rounded-t-lg"
+                      />
+                      <div className="h-32 p-4">
+                        <div className="text-2xl font-bold text-neutral-200 line-clamp-2">
+                          {item.title}
+                        </div>
+                        <div className="text-neutral-500 line-clamp-3">
+                          {item.description}
+                        </div>
+                      </div>
+                    </div>
+                  </a>
+                </Link>
+              ))}
         </div>
       </div>
     </>
